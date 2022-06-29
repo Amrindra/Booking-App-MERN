876e78a32e15580c9ffdcebbb97cae8861d38007
Amrindra Ouk <amrindraouk@yahoo.com>
2022-06-29 14:16:44 -0400

    settup mongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 
-dotenv.confige();
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log("Connected to DB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+// if something went wrong disconnect the DB
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected");
+});
+
+// if something is okay connect the DB again
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected");
+});
 
 app.listen(8000, () => {
+  connectDB();
   console.log("Server is running on port 8000");
 });
