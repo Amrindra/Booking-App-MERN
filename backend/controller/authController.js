c7f6e35d04e993691adcf80835cf09c7b0fc44fe
Amrindra Ouk <amrindraouk@yahoo.com>
2022-07-07 11:42:57 -0400

    Installed bcryptjs and setup hash for authController

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,12 +1,20 @@
 const UserModel = require("../models/UserModel.js");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res, next) => {
   try {
+    // Using bcryptjs to hash password
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new UserModel({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashPassword,
     });
+
+    await newUser.save();
+    res.status(200).send("User have been registered!");
   } catch (error) {
     next(error);
   }
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -3,6 +3,6 @@ const register = require("../controller/authController");
 
 const router = express.Router();
 
-router.get("/register", register);
+router.post("/register", register);
 
 module.exports = router;
