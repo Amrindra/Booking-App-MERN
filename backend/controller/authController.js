5fc34025fa422b9519e84f9abf881562fbfbd8e2
Amrindra Ouk <amrindraouk@yahoo.com>
2022-07-11 11:09:32 -0400

    Implemented login route API

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,8 @@
 const UserModel = require("../models/UserModel.js");
 const bcrypt = require("bcryptjs");
+const createError = require("../utils/error.js");
 
+//Register
 const register = async (req, res, next) => {
   try {
     // Using bcryptjs to hash password
@@ -20,4 +22,28 @@ const register = async (req, res, next) => {
   }
 };
 
-module.exports = register;
+// Login
+const login = async (req, res, next) => {
+  try {
+    //Finding current user in the database if not found send back the "User not found"
+    const currentUser = await UserModel.findOne({
+      username: req.body.username,
+    });
+    if (!currentUser) return next(createError(404, "User not found"));
+
+    //Comparing the password from user types in and password in the database
+    //If it doesn't match send error message, if it does send that user
+    const checkingPassword = await bcrypt.compare(
+      req.body.password,
+      currentUser.password
+    );
+    if (!checkingPassword)
+      return next(createError(400, "Wrong password or username!"));
+
+    res.status(200).json(currentUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { register, login };
