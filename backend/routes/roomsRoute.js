425499715e1f26dee9c861210f601a2a9e324bc2
Amrindra Ouk <amrindraouk@yahoo.com>
2022-07-20 15:59:12 -0400

    Created Room Controller

--- backend/routes/roomsRoute.js
+++ backend/routes/roomsRoute.js
@@ -1,5 +1,32 @@
 const express = require("express");
+const {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+  getAllHotels,
+} = require("../controller/hotelController.js");
+const HotelModel = require("../models/HotelModel.js");
+const createError = require("../utils/error.js");
+const { verifyUser, verifyAdmin } = require("../utils/verifyToken");
 
 const router = express.Router();
+
+// CREATE
+// Only admin can create hotel that's why we passed only verifyAdmin here
+router.post("/", verifyAdmin, create);
+
+// UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+
+// DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+
+// GET SPECIFIC HOTEL
+router.get("/:id", getHotel);
+
+//GET ALL
+//Used next middleware so that we can customize our error messages
+router.get("/", getAllHotels);
 
 module.exports = router;
