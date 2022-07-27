6ff9e8d8d930a53027310f019e7ab9de7cd14f23
Amrindra Ouk <amrindraouk@yahoo.com>
2022-07-27 11:28:28 -0400

    Gave condition to properties

--- frontend/src/components/heroBanner/HeroBanner.jsx
+++ frontend/src/components/heroBanner/HeroBanner.jsx
@@ -3,21 +3,22 @@ import "./HeroBanner.scss";
 
 const HeroBanner = () => {
 
-  const { data, loading, error } = useFetch("/hotels/countByCity?cities=Berlin,Madrid,London")
+  const { data, loading, error } = useFetch("/hotels/countByCity?cities=Boston,Maine,Haverhill")
 
   console.log(data);
 
   return (
     <div className="hero_banner">
-      <div className="hero_banner_item">
+      {loading ? "Loading data..." :<><div className="hero_banner_item">
         <img
           src="https://a.cdn-hotels.com/gdcs/production79/d1680/d7d5bf20-9335-409b-b6a5-878f5c502e86.jpg"
           alt=""
           className="hero_banner_image"
         />
         <div className="hero_banner_titles">
-          <h1>New Hamshire</h1>
-          <h2>123 new hamshire</h2>
+          <h1>Boston</h1>
+          {/* This condition is used to show property if it has only 1 show Property if it's more than 1 show Properties */}
+          <h2>{`${data[0] > 1? `${data[0]} Properties`: `${data[0]} Property`}`}</h2>
         </div>
       </div>
 
@@ -28,8 +29,8 @@ const HeroBanner = () => {
           className="hero_banner_image"
         />
         <div className="hero_banner_titles">
-          <h1>New Hamshire</h1>
-          <h2>123 new hamshire</h2>
+          <h1>Maine</h1>
+          <h2>{`${data[1] > 1? `${data[1]} Properties`: `${data[1]} Property`}`}</h2>
         </div>
       </div>
       <div className="hero_banner_item">
@@ -39,10 +40,10 @@ const HeroBanner = () => {
           className="hero_banner_image"
         />
         <div className="hero_banner_titles">
-          <h1>New Hamshire</h1>
-          <h2>123 new hamshire</h2>
+          <h1>Haverhill</h1>
+          <h2>{`${data[2] > 1? `${data[2]} Properties`: `${data[2]} Property`}`}</h2>
         </div>
-      </div>
+      </div></>}
     </div>
   );
 };
