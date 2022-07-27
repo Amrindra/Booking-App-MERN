e7df1840ae3b57b8b46e32ae5cd9a9dcf135a829
Amrindra Ouk <amrindraouk@yahoo.com>
2022-07-27 00:38:12 -0400

    Implemented a custom hook for fetching api from the server

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  //When we're loading an API request, the loading state will be true. If there's data it will store in the data state. otherwise it will set to error state
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      //While fetching data, setLoading to true
+      setLoading(true);
+      const response = await axios.get(url);
+      try {
+        setData(response.data);
+      } catch (error) {
+        setError(true);
+      }
+
+      //When fetching is done setLoading to false
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetchData = async () => {
+    //While fetching data, setLoading to true
+    setLoading(true);
+    const response = await axios.get(url);
+    try {
+      setData(response.data);
+    } catch (error) {
+      setError(true);
+    }
+
+    //When fetching is done setLoading to false
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetchData };
+};
+
+export default useFetch;
