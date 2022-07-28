feafd61f1777e5ccdefeaa0c927c6e44e670d7c1
Amrindra Ouk <amrindraouk@yahoo.com>
2022-07-28 11:32:07 -0400

    Refactored PropertyList code and fetched data from server for this component as well

--- frontend/src/components/propertyList/PropertyList.jsx
+++ frontend/src/components/propertyList/PropertyList.jsx
@@ -1,62 +1,38 @@
+import useFetch from "../../hooks/useFetch";
 import "./PropertyList.scss";
 
 const PropertyList = () => {
+  const { data, loading, error } = useFetch("/hotels/countByType")
+
+  console.log(data)
+
+  const images = [
+    "https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1522798514-97ceb8c4f1c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80",
+  ]
+
   return (
     <div className="prop_list">
-      <div className="prop_items">
-        <img
-          src="https://static.onecms.io/wp-content/uploads/sites/28/2021/02/23/boston-massachusetts-BOSTONTG0221.jpg"
-          alt=""
-        />
-        <div className="prop_titles">
-          <h1>Hotels</h1>
-          <h2>233 Hotels</h2>
-        </div>
-      </div>
+      {loading ? "Loading data..." :
+        <>
+          {data && images.map((image,index) => (
 
-      <div className="prop_items">
-        <img
-          src="https://cdn.getyourguide.com/img/location/533597d7653a9.jpeg/99.jpg"
-          alt=""
-        />
-        <div className="prop_titles">
-          <h1>Apartments</h1>
-          <h2>233 Hotels</h2>
-        </div>
-      </div>
+            <div className="prop_items">
+              <img
+                src={image}
+                alt=""
+              />
+              <div className="prop_titles">
+                <h1>{data[index].type}</h1>
+                <h2>{data[index].count} {data[index].type}</h2>
+              </div>
+            </div>))}
 
-      <div className="prop_items">
-        <img
-          src="https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2020/10/boston-foliage-social.jpg"
-          alt=""
-        />
-        <div className="prop_titles">
-          <h1>Resorts</h1>
-          <h2>233 Hotels</h2>
-        </div>
-      </div>
 
-      <div className="prop_items">
-        <img
-          src="https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2020/10/boston-foliage-social.jpg"
-          alt=""
-        />
-        <div className="prop_titles">
-          <h1>Villas</h1>
-          <h2>233 Hotels</h2>
-        </div>
-      </div>
-
-      <div className="prop_items">
-        <img
-          src="https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2020/10/boston-foliage-social.jpg"
-          alt=""
-        />
-        <div className="prop_titles">
-          <h1>Carbin</h1>
-          <h2>233 Hotels</h2>
-        </div>
-      </div>
+        </>}
     </div>
   );
 };
