a51dd182758ee3b4333a0b26125cd9ca10da9841
Amrindra Ouk <amrindraouk@yahoo.com>
2022-07-29 15:13:01 -0400

    set up the FeatureHotel component by fetching the data from the server

--- frontend/src/components/featureHotel/FeatureHotel.jsx
+++ frontend/src/components/featureHotel/FeatureHotel.jsx
@@ -4,56 +4,39 @@ import "./FeatureHotel.scss";
 // import { faStarSharp } from "@fortawesome/free-solid-svg-icons";
 
 const FeatureHotel = () => {
-  const countByTypeURL = "/hotels/countByCity?cities=Boston,Maine,Haverhill";
-  const { data, loading } = useFetch(countByTypeURL);
+  const { data, loading } = useFetch(
+    "/hotels?featured=true&limit=5&min=10&max=200"
+  );
 
   return (
     <div className="feature_hotel">
-      <div className="feature_hotel_items">
-        <img
-          src="https://media-cdn.tripadvisor.com/media/photo-s/1c/d2/51/82/exterior.jpg"
-          alt=""
-        />
-        <span className="feature_hotel_name">Happy Inn</span>
-        <span className="feature_hotel_city">Boston</span>
-        <span className="feature_hotel_price">$200/night</span>
+      {loading ? (
+        "Loading data..."
+      ) : (
+        <>
+          {data.map((item) => (
+            <div className="feature_hotel_items" key={item._id}>
+              <img
+                src="https://media-cdn.tripadvisor.com/media/photo-s/1c/d2/51/82/exterior.jpg"
+                alt=""
+              />
+              <span className="feature_hotel_name">{item.hotelName}</span>
+              <span className="feature_hotel_city">{item.city}</span>
+              <span className="feature_hotel_price">
+                ${item.cheapestPrice}/night
+              </span>
 
-        <div className="feature_hotel_rating">
-          <button>5</button>
-          <span>Excellent</span>
-        </div>
-      </div>
-
-      <div className="feature_hotel_items">
-        <img
-          src="https://media-cdn.tripadvisor.com/media/photo-s/21/5e/3c/e0/exterior-view.jpg"
-          alt=""
-        />
-        <span className="feature_hotel_name">Happy Inn</span>
-        <span className="feature_hotel_city">Boston</span>
-        <span className="feature_hotel_price">$200/night</span>
-
-        <div className="feature_hotel_rating">
-          {/* <FontAwesomeIcon icon={faStarSharp} /> */}
-          <button>5</button>
-          <span>Excellent</span>
-        </div>
-      </div>
-
-      <div className="feature_hotel_items">
-        <img
-          src="https://www.thehotelguru.com/_images/6e/9c/6e9c275a8216784cefd71df1a093f051/600x422.jpg"
-          alt=""
-        />
-        <span className="feature_hotel_name">Happy Inn</span>
-        <span className="feature_hotel_city">Boston</span>
-        <span className="feature_hotel_price">$200/night</span>
-
-        <div className="feature_hotel_rating">
-          <button>5</button>
-          <span>Excellent</span>
-        </div>
-      </div>
+              {/* If there is rating it will here accordingly */}
+              {item.rating && (
+                <div className="feature_hotel_rating">
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>
+              )}
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
