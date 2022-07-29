f28f0507af8d1fb36144e85cd7dfc1f23300436e
Amrindra Ouk <amrindraouk@yahoo.com>
2022-07-29 13:44:25 -0400

    test

--- frontend/src/components/propertyList/PropertyList.jsx
+++ frontend/src/components/propertyList/PropertyList.jsx
@@ -2,9 +2,9 @@ import useFetch from "../../hooks/useFetch";
 import "./PropertyList.scss";
 
 const PropertyList = () => {
-  const { data, loading } = useFetch("/hotels/countByType")
+  const { data, loading } = useFetch("/hotels/countByType");
 
-  console.log(data)
+  // console.log(data)
 
   const images = [
     "https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
@@ -12,27 +12,28 @@ const PropertyList = () => {
     "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80",
-  ]
+  ];
 
   return (
     <div className="prop_list">
-      {loading ? "Loading data..." :
+      {loading ? (
+        "Loading data..."
+      ) : (
         <>
-          {data && images.map((image,index) => (
-
-            <div className="prop_items" key={index}>
-              <img
-                src={image}
-                alt="hotel pictures"
-              />
-              <div className="prop_titles">
-                <h1>{data[index]?.type}</h1>
-                <h2>{data[index]?.count} {data[index]?.type}</h2>
+          {data &&
+            images.map((image, index) => (
+              <div className="prop_items" key={index}>
+                <img src={image} alt="hotel pictures" />
+                <div className="prop_titles">
+                  <h1>{data[index]?.type}</h1>
+                  <h2>
+                    {data[index]?.count} {data[index]?.type}
+                  </h2>
+                </div>
               </div>
-            </div>))}
-
-
-        </>}
+            ))}
+        </>
+      )}
     </div>
   );
 };
